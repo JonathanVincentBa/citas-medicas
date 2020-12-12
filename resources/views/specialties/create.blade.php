1371e499f3699f740fe2499ee70cc665f8ec22a7
Jonathan Vincent <jonathanvincent@outlook.com>
2020-12-12 00:08:50 -0500

    rectificacion de la plantilla

---
+++ resources/views/specialties/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.panel')
+
+@section('title', 'Crear')
+
+@section('content')
+<div class="card mb-5">
+    <div class="card-header border-0">
+        <div class="row align-items-center">
+            <div class="col">
+                <h3 class="mb-0">Nueva Especialidad</h3>
+            </div>
+            <div class="col text-right">
+                <a href="{{ url('specialties') }}" class="btn btn-sm btn-default">
+                    Cancelar y volver
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </div>
+        @endif
+        <form action="{{ url('specialties') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Nombre de la especialidad</label>
+                <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="description">Descripción de la especialidad</label>
+                <input type="text" name="description" value="{{ old('description') }}" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
+</div>
+@endsection
