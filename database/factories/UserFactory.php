f7d4ef2fad63f6a64253382e7f57c12dc67b3378
Jonathan Vincent <jonathanvincent@outlook.com>
2020-12-12 01:39:57 -0500

    actualizando la tabla user y llenando con 50 users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,5 +21,9 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
+        'dni'=> $faker->unique()->numberBetween($min = 1000000000, $max = 9999999999),
+        'address' => $faker->address,
+        'phone' => $faker->e164PhoneNumber,
+        'role' =>$faker->randomElement(['patient', 'doctor'])
     ];
 });
